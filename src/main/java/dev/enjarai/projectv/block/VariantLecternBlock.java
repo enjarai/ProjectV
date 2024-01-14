141db1df48b741fe233d5929cc7e30311136ede0
enjarai <enjarai123@gmail.com>
2024-01-14 21:34:36 +0100

    Work in proges

---
+++ src/main/java/dev/enjarai/projectv/block/VariantLecternBlock.java
@@ -0,0 +1,9 @@
+package dev.enjarai.projectv.block;
+
+import net.minecraft.block.LecternBlock;
+
+public class VariantLecternBlock extends LecternBlock implements VariantBlock {
+    public VariantLecternBlock(Settings settings) {
+        super(settings);
+    }
+}
