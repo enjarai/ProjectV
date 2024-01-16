95c53dbfd60d027f0993e2ddc02ad02ec0763a7a
D1p4k <82884007+D1p4k@users.noreply.github.com>
2024-01-16 22:55:34 +0100

    I guess I need that mixin, but better :)

---
+++ src/main/java/dev/enjarai/projectv/mixin/BlockEntityTypeMixin.java
@@ -0,0 +1,32 @@
+package dev.enjarai.projectv.mixin;
+
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import dev.enjarai.projectv.block.VariantBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntityType;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Set;
+
+/**
+ * General mixin to modify blockentity types to support our variants.
+ */
+@Mixin(BlockEntityType.class)
+public class BlockEntityTypeMixin {
+    @Shadow
+    @Final
+    private Set<Block> blocks;
+
+    @ModifyReturnValue(
+            method = "supports",
+            at = @At("RETURN")
+    )
+    private boolean addSupportedBlocks(boolean original, BlockState state) {
+        return original || state.getBlock() instanceof VariantBlock variantBlock && this.blocks.contains(variantBlock.getBaseBlock());
+    }
+}
