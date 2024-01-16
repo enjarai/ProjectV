6c29de7443b146a70f7b6acd1f472894839baa72
enjarai <enjarai123@gmail.com>
2024-01-16 23:56:42 +0100

    Incredible

--- src/client/java/dev/enjarai/projectv/ProjectVClient.java
+++ src/client/java/dev/enjarai/projectv/ProjectVClient.java
@@ -1,8 +1,12 @@
 package dev.enjarai.projectv;
 
+import dev.enjarai.projectv.block.VariantBlock;
 import dev.enjarai.projectv.pack.PackAdderEvent;
 import dev.enjarai.projectv.resource.BlockVariantTextureGenerator;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayers;
+import net.minecraft.registry.Registries;
 import net.minecraft.resource.ResourceType;
 
 public class ProjectVClient implements ClientModInitializer {
@@ -11,6 +15,13 @@ public class ProjectVClient implements ClientModInitializer {
 		PackAdderEvent.EVENT.register((managerType, packs) -> {
 			if (managerType == ResourceType.CLIENT_RESOURCES) {
 				packs.add(BlockVariantTextureGenerator.PACK);
+			}
+		});
+
+		Registries.BLOCK.forEach(block -> {
+			if (block instanceof VariantBlock variantBlock) {
+				BlockRenderLayerMap.INSTANCE.putBlock(block,
+						RenderLayers.getBlockLayer(variantBlock.getBaseBlock().getDefaultState()));
 			}
 		});
 	}
