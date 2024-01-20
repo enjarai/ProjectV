8b2706cee7a19a84ad92657f1762ed54feb00a6b
enjarai <enjarai123@gmail.com>
2024-01-20 12:25:00 +0100

    Cicada jazz innit

--- src/main/java/dev/enjarai/projectv/ProjectV.java
+++ src/main/java/dev/enjarai/projectv/ProjectV.java
@@ -8,15 +8,19 @@ import net.fabricmc.api.ModInitializer;
 import net.minecraft.registry.Registry;
 import net.minecraft.resource.ResourceType;
 import net.minecraft.util.Identifier;
+import nl.enjarai.cicada.api.conversation.ConversationManager;
+import nl.enjarai.cicada.api.util.CicadaEntrypoint;
+import nl.enjarai.cicada.api.util.JsonSource;
+import nl.enjarai.cicada.api.util.ProperLogger;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Objects;
 
-public class ProjectV implements ModInitializer {
+public class ProjectV implements ModInitializer, CicadaEntrypoint {
 
 	public static final String MOD_ID = "projectv";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static final Logger LOGGER = ProperLogger.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
@@ -31,6 +35,15 @@ public class ProjectV implements ModInitializer {
 				packs.add(BlockVariantTagGenerator.PACK);
 			}
 		});
+	}
+
+	@Override
+	public void registerConversations(ConversationManager conversationManager) {
+		conversationManager.registerSource(
+				JsonSource.fromUrl("https://raw.githubusercontent.com/enjarai/ProjectV/master/src/main/resources/cicada/projectv/conversations.json")
+						.or(JsonSource.fromResource("cicada/projectv/conversations.json")),
+				LOGGER::info
+		);
 	}
 
 	public static Identifier id(String path) {
